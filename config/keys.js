fab2d95694c89a450135a5d3a116eb8b479112fc
Jason Belgard <belgard.jason@gmail.com>
2020-01-17 04:06:20 -0600

    Routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:Amelia80@cluster0-et8gp.gcp.mongodb.net/test?retryWrites=true&w=majority"
-}
+  mongoURI: "mongodb+srv://admin:Amelia80@cluster0-et8gp.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
